982bb5687a323ee5dbd9a4302e947aa80e5b608a
oztasozgurcan <oztas.ozgurcan@gmail.com>
2019-05-01 18:28:39 +0300

    CRD operations completed.

--- blazeApplication/src/main/java/io/blaze/blazeApplication/model/Controller.java
+++ blazeApplication/src/main/java/io/blaze/blazeApplication/model/Controller.java
@@ -4,44 +4,35 @@ import io.blaze.blazeApplication.repository.*;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-
-import javax.sql.DataSource;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
-	@Autowired
-	private DataSource datasource;
+	
 	@Autowired 
 	private AppRepository appRepository;
 	@Autowired
 	private Service service;
 	
-	private Map<String, Integer> name_id_map;
-	
-	@RequestMapping("/index")
+	@GetMapping("/index")
 	public String indexloader() {
 		return "Index page.";
 	}
 	
-	@RequestMapping("/{name}")
-	public String repositoryLoader(@PathVariable String name) throws DataAccessException{
-		if(this.findbyUserInfobyName(name)) {
-			return "This user is already imported.";
-		}
-		String result = service.repositoryLoader(name);
-		appRepository.save(new User(name, "Successful."));
-		return "Successful";
-	}
-	@RequestMapping("/all")
+	@GetMapping("/users")
 	public String getAllUserInfo() {
 		String result = "";
 		Iterable<User> current = appRepository.findAll();
@@ -52,21 +43,45 @@ public class Controller {
 		return result.toString();
 	}
 	
-	public boolean findbyUserInfobyName(String name) {
-		Iterable<User> user_list = appRepository.findAll();
-		Iterator<User> ite = user_list.iterator();
-		for(;ite.hasNext();) {
-			User current = ite.next();
-			if(current.getName().toString().equals(name)) {
-				return true;
-			}
-		}
-		return false;
+	@GetMapping("/find/{name}")
+	public String findUserbyName(@PathVariable String name) {
+		Optional<User> current_user_data = appRepository.findByName(name);
+		User current_user = current_user_data.get();
+		String result = "";
+		result += current_user.getName() + ", " + 
+		current_user.getId() + ", " +current_user.getRepository_Info() +"\n";	
+		return result;
 	}
 	
-	@RequestMapping("/clearall")
-	public String deleteAllUserInfo() {
-		appRepository.deleteAll();
-		return "Successful.";
+	@PostMapping("/users/{name}")
+	public String repositoryLoader(@PathVariable String name) throws DataAccessException{
+		if(this.findbyUserInfobyName(name) != null) {
+			return "This user is already imported.";
+		}
+		String result = service.repositoryLoader(name);
+		appRepository.save(new User(name, "Successful."));
+		return result;
 	}
+	
+	@DeleteMapping("/users/{name}")
+	public ResponseEntity<String> deleteCustomerbyName(@PathVariable String name){
+		User deleted_user = findbyUserInfobyName(name);
+		appRepository.deleteById(deleted_user.getId());
+		return new ResponseEntity<>("All user data is deleted.", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteall")
+	public ResponseEntity<String> deleteAllUserInfo() {
+		appRepository.deleteAll();
+		return new ResponseEntity<>("User database is completely cleared.", HttpStatus.OK);
+	}
+	
+	public User findbyUserInfobyName(String name) {
+		Optional<User> desired_user = appRepository.findByName(name);
+		if(desired_user.isPresent()) {
+			return desired_user.get();
+		}
+		return null;
+	}
+	
 }
--- blazeApplication/src/main/java/io/blaze/blazeApplication/repository/AppRepository.java
+++ blazeApplication/src/main/java/io/blaze/blazeApplication/repository/AppRepository.java
@@ -1,10 +1,12 @@
 package io.blaze.blazeApplication.repository;
 
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 import io.blaze.blazeApplication.model.User;
 
 public interface AppRepository extends CrudRepository<User, Integer>{
-
+	Optional<User> findByName(String name);
 }
